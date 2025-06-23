73bb7c18951074043d8ae7c08b9668dd36247e8a
Arthur Fontaine <0arthur.fontaine@gmail.com>
2025-06-23 15:03:41 +0200

    Add support for nullable single reference and improve key typing of single reference

--- src/single-reference.ts
+++ src/single-reference.ts
@@ -4,25 +4,28 @@ import type { Table } from "./table.ts";
 
 declare const __singleReference: unique symbol;
 
-export type SingleReference<T, IN extends string, OUT extends keyof T> = Special<T & {
+export type SingleReference<T, IN extends string, OUT extends keyof NonNullable<T>> = Special<T & {
   [__singleReference]: {
     input: IN;
     output: OUT;
+    type: T;
   };
 }>;
 
 export type SelectableSingleReference<T extends Table> =
   {
     [K in keyof T as T[K] extends SingleReference<unknown, string, any> ? K : never]:
-    T[K] extends SingleReference<infer U, string, any> ? ISU.Selectable<U> : never;
+    T[K] extends SingleReference<infer U, string, any> ? PotentialNullable<U, ISU.Selectable<U>> : never;
   } &
   {
     [K in keyof T as T[K] extends SingleReference<unknown, infer IN, any> ? IN : never]:
-    T[K] extends SingleReference<infer S, string, infer OUT> ? ISU.Selectable<{ '_': S[OUT] }>['_'] : never;
+    T[K] extends SingleReference<infer S, string, infer OUT> ? PotentialNullable<S, ISU.Selectable<{ '_': S[OUT] }>['_']> : never;
   };
 
 export type InsertableSingleReference<T extends Table> =
   {
     [K in keyof T as T[K] extends SingleReference<unknown, infer IN, any> ? IN : never]:
-    T[K] extends SingleReference<infer S, string, infer OUT> ? ISU.Selectable<{ '_': S[OUT] }>['_'] : never;
+    T[K] extends SingleReference<infer S, string, infer OUT> ? PotentialNullable<S, ISU.Selectable<{ '_': S[OUT] }>['_']> : never;
   }
+
+type PotentialNullable<T, REAL_TYPE> = null extends T ? REAL_TYPE | null : REAL_TYPE;
