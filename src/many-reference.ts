aacd21321985584a08d9906945fd56a76287fdf0
Arthur Fontaine <0arthur.fontaine@gmail.com>
2025-06-28 01:44:55 +0200

    Fix `Type 'U' does not satisfy the constraint 'Table'.`

--- src/many-reference.ts
+++ src/many-reference.ts
@@ -4,7 +4,7 @@ import type { Table } from "./table.ts";
 
 declare const __manyReference: unique symbol;
 
-export type ManyReference<T> = Special<T & {
+export type ManyReference<T extends Table> = Special<T & {
   [__manyReference]: {
     type: T;
   }
@@ -12,11 +12,13 @@ export type ManyReference<T> = Special<T & {
 
 export type SelectableManyReference<T extends Table> =
   {
+    // @ts-expect-error unknown is used to allow for any type of input
     [K in keyof T as T[K] extends ManyReference<unknown> ? K : never]:
     T[K] extends ManyReference<infer U> ? ISU.Selectable<U>[] : never;
   };
 
 export type InsertableManyReference<T extends Table> =
   {
+    // @ts-expect-error unknown is used to allow for any type of input
     [K in keyof T as T[K] extends ManyReference<unknown> ? never : never]: T[K];
   }
